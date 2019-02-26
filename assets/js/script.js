5fdd3e233f93dfce8556dcdbe0f5ae976709d47a
cevaho <cevaho.creation@gmail.com>
2019-02-26 15:21:21 +0100

    ajout de service en js pour PWA

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,24 @@
 (function() {
+
+
+// register service worker - BEGINNING
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/bootstrap-resto-website/sw.js', { scope: '/bootstrap-resto-website/' }).then(function(reg) {
+
+    if(reg.installing) {
+      console.log('Service worker installing');
+    } else if(reg.waiting) {
+      console.log('Service worker installed');
+    } else if(reg.active) {
+      console.log('Service worker active');
+    }
+
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+}
+// register service worker - END
     
 //s'activera dans la page index :
 // au clic sur le bouton #run affiche le nom d'user dans le jumbotron
